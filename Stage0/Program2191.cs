f1366b5ef8d00b123a663e8ffdffb271332e59b2
Gitty Zinger <GZinger@synamedia.com>
2024-11-06 17:02:27 +0200

    start stage 1

---
+++ Stage0/Program2191.cs
@@ -0,0 +1,22 @@
+﻿using System
+namespace Targil0 {
+
+    partial class Program
+{
+     static void Main(string[] args)
+        {
+            Welcom2191();
+            WelcomYYYY();
+
+            Console.ReadKey();
+        }
+        partial static void WelcomYYYY()
+
+        private static void Welcom2191()
+        {
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Enter your name: ");
+            string UserName = Console.ReadLine();
+            console.WriteLine("{0}, welcome to my first console application", UserName);
+        }
+    }
